c230f7b9120f911a8817fe9e53bdbdc4541c3a23
Huei Tan <huei90@gmail.com>
2021-04-27 20:18:48 +0200

    add wikipedia-preview dependency and enqueue script from it

--- wikipediapreview-wordpress.php
+++ wikipediapreview-wordpress.php
@@ -6,19 +6,17 @@
  */
 
 function wikipediapreview_enqueue_scripts() {
-    // @todo insert script from node_modules
 	wp_enqueue_script(
         'wikipedia-preview', 
-        'https://unpkg.com/wikipedia-preview@1.1.1/dist/wikipedia-preview.production.js', [], null, true
+        plugin_dir_url( __FILE__ ) . 'node_modules/wikipedia-preview/dist/wikipedia-preview.development.js', [], false, true 
     );
 
     wp_enqueue_style(
 		'wmf-wp-format-css',
-		'https://unpkg.com/wikipedia-preview@1.1.1/dist/wikipedia-preview.css'
+        plugin_dir_url( __FILE__ ) . 'node_modules/wikipedia-preview/dist/wikipedia-preview.css', [], false, true 
 	);
 
-	// The content of the init file is basically `wikipediaPreview.init({});`
-	wp_enqueue_script('wikipedia-preview-init', plugin_dir_url( __FILE__ ) . '/init.js', [], false, true);	
+	wp_enqueue_script('wikipedia-preview-init', plugin_dir_url( __FILE__ ) . 'init.js', [], false, true);	
 }
 
 add_action( 'wp_enqueue_scripts', 'wikipediapreview_enqueue_scripts' );
