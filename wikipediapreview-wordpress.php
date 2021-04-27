3caed5d41fe2909b854813b4b0235343c554dafe
Huei Tan <huei90@gmail.com>
2021-04-27 20:20:05 +0200

    lint between space / tab

--- wikipediapreview-wordpress.php
+++ wikipediapreview-wordpress.php
@@ -6,17 +6,17 @@
  */
 
 function wikipediapreview_enqueue_scripts() {
-	wp_enqueue_script(
+    wp_enqueue_script(
         'wikipedia-preview', 
         plugin_dir_url( __FILE__ ) . 'node_modules/wikipedia-preview/dist/wikipedia-preview.development.js', [], false, true 
     );
 
     wp_enqueue_style(
-		'wmf-wp-format-css',
+        'wmf-wp-format-css',
         plugin_dir_url( __FILE__ ) . 'node_modules/wikipedia-preview/dist/wikipedia-preview.css', [], false, true 
-	);
+    );
 
-	wp_enqueue_script('wikipedia-preview-init', plugin_dir_url( __FILE__ ) . 'init.js', [], false, true);	
+    wp_enqueue_script('wikipedia-preview-init', plugin_dir_url( __FILE__ ) . 'init.js', [], false, true);	
 }
 
 add_action( 'wp_enqueue_scripts', 'wikipediapreview_enqueue_scripts' );
