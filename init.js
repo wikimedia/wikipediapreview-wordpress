75d296fbdd606b3bf37838b1a6b370c3a55802a5
Huei Tan <huei90@gmail.com>
2021-04-27 05:10:01 +0200

    Copy & paste selected code from the wpwpp

---
+++ init.js
@@ -0,0 +1,5 @@
+wikipediaPreview.init({
+	root: document.querySelector('.entry-content'),
+	lang: 'en', // @todo wordpress site language
+	detectLinks: true
+});
--- wikipediapreview-wordpress.php
+++ wikipediapreview-wordpress.php
@@ -4,3 +4,21 @@
  * Description: Add Wikipedia Preview to your articles
  * Version: 0.1.0
  */
+
+function wikipediapreview_enqueue_scripts() {
+    // @todo insert script from node_modules
+	wp_enqueue_script(
+        'wikipedia-preview', 
+        'https://unpkg.com/wikipedia-preview@1.1.1/dist/wikipedia-preview.production.js', [], null, true
+    );
+
+    wp_enqueue_style(
+		'wmf-wp-format-css',
+		'https://unpkg.com/wikipedia-preview@1.1.1/dist/wikipedia-preview.css'
+	);
+
+	// The content of the init file is basically `wikipediaPreview.init({});`
+	wp_enqueue_script('wikipedia-preview-init', plugin_dir_url( __FILE__ ) . '/init.js', [], false, true);	
+}
+
+add_action( 'wp_enqueue_scripts', 'wikipediapreview_enqueue_scripts' );
