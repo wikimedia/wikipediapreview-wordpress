b2c1eaa899e4c16f20de8d2b25d160d9d3a8fe63
Huei Tan <huei90@gmail.com>
2021-04-27 21:11:27 +0200

    add "wikipedia_preview_options" option to record the auto_detects feature; for backward compatibility, the plugin knows the detect links has been enabled in the earlier version

--- wikipediapreview-wordpress.php
+++ wikipediapreview-wordpress.php
@@ -16,7 +16,15 @@ function wikipediapreview_enqueue_scripts() {
         plugin_dir_url( __FILE__ ) . 'node_modules/wikipedia-preview/dist/wikipedia-preview.css', [], false, true 
     );
 
-    wp_enqueue_script('wikipedia-preview-init', plugin_dir_url( __FILE__ ) . 'init.js', [], false, true);	
+    wp_enqueue_script( 'wikipedia-preview-init', plugin_dir_url( __FILE__ ) . 'init.js', [], false, true );	
 }
 
+// record the option of detect links feature enabled in this version,
+// detect links feature may disable by default in the next version
+function record_options() {
+    $options = array( 'auto_detects' => true );
+    add_option( 'wikipedia_preview_options', $options );
+}
+
+add_action( 'init', 'record_options' );
 add_action( 'wp_enqueue_scripts', 'wikipediapreview_enqueue_scripts' );
