0b7272ba5e38641c1c1dba27362d78016066f458
Eduardo <medina.eag@gmail.com>
2021-04-28 11:28:53 -0400

    Update header fields

--- wikipediapreview-wordpress.php
+++ wikipediapreview-wordpress.php
@@ -4,7 +4,10 @@
  * Plugin URI: https://github.com/wikimedia/wikipedia-preview
  * Description: Wikipedia Preview allows you to show a popup card with a short summary from Wikipedia when a reader clicks or hovers over a link
  * Version: 1.1.1
+ * Requires at least: 5.6
+ * Requires PHP: 8.0.2
  * Author: Wikimedia Foundation
+ * Author URI: https://wikimediafoundation.org/
  * License: MIT
  * License URI: https://github.com/wikimedia/wikipedia-preview/blob/main/LICENSE
  */
