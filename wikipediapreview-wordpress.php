74b8be9076978caf63f57b24fe5b8332747b985e
Eduardo <medina.eag@gmail.com>
2021-04-28 21:36:50 -0400

    Update software version requirements

--- wikipediapreview-wordpress.php
+++ wikipediapreview-wordpress.php
@@ -4,8 +4,8 @@
  * Plugin URI: https://github.com/wikimedia/wikipedia-preview
  * Description: Wikipedia Preview allows you to show a popup card with a short summary from Wikipedia when a reader clicks or hovers over a link
  * Version: 1.1.1
- * Requires at least: 5.6
- * Requires PHP: 8.0.2
+ * Requires at least: 4.2
+ * Requires PHP: 5.6.39
  * Author: Wikimedia Foundation
  * Author URI: https://wikimediafoundation.org/
  * License: MIT
