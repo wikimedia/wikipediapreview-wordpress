8405c65d1da140a6f600ee149c5aec93cd2a4751
Huei Tan <hueitan@users.noreply.github.com>
2021-04-30 16:04:24 +0200

    T280000 Plugin detect links  (#6)
    * Copy & paste selected code from the wpwpp
    
    * lang is unnecessary as detect links has the lang info
    
    * add wikipedia-preview dependency and enqueue script from it
    
    * lint between space / tab
    
    * add "wikipedia_preview_options" option to record the auto_detects feature; for backward compatibility, the plugin knows the detect links has been enabled in the earlier version
    
    * update bugs url to phabricator link
    
    * add postinstall script and .gitignore
    
    * code review and remove css
    
    * hook for the plugin activation, deactivation and uninstall
    
    * moving script into assets/js
    
    * lint
    
    * unnecessary for the uninstall hook
    
    * add "Run the plugin locally" in README.md

--- wikipediapreview-wordpress.php
+++ wikipediapreview-wordpress.php
@@ -4,3 +4,28 @@
  * Description: Add Wikipedia Preview to your articles
  * Version: 0.1.0
  */
+function wikipediapreview_enqueue_scripts() {
+    $assets_dir = plugin_dir_url( __FILE__ ) . 'assets/';
+    
+    wp_enqueue_script(
+        'wikipedia-preview', $assets_dir . 'js/wikipedia-preview.production.js', [], false, true 
+    );
+
+    wp_enqueue_script( 
+        'wikipedia-preview-init', $assets_dir . 'js/init.js', [], false, true 
+    );	
+}
+
+// record the option of detect links feature enabled in this version,
+// detect links feature may disable by default in the next version
+function wikipediapreview_detect_true() {
+    add_option( 'wikipediapreview_options_detect_links', true );
+}
+
+function wikipediapreview_detect_deletion() {
+    delete_option( 'wikipediapreview_options_detect_links' );
+}
+
+register_activation_hook( __FILE__, 'wikipediapreview_detect_true' );
+register_deactivation_hook( __FILE__, 'wikipediapreview_detect_deletion' );
+add_action( 'wp_enqueue_scripts', 'wikipediapreview_enqueue_scripts' );
