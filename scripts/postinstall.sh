d3b328903f563e61543b58112e8adf91b3dcbf12
Huei Tan <huei90@gmail.com>
2021-05-05 16:16:35 +0200

    excute build script and moving production js from wikipedia-preview to assets

--- scripts/postinstall.sh
+++ scripts/postinstall.sh
@@ -1,13 +1,19 @@
 #!/bin/bash
 
 mkdir -p assets/images
-
 cp wikipedia-preview/images/* assets/images
 
 # rewrite the replace method in a better format
-sed -i "s/inline-svg( '..\/images/url( wp-content\/plugins\/wikipediapreview-wordpress\/assets\/images/g" wikipedia-preview/style/*.less
-sed -i "s/svg' )/svg )/g" wikipedia-preview/style/*.less
-sed -i "s/url( '/url( /g" wikipedia-preview/style/*.less
-sed -i "s/import '..\/style\/.*.less'//g" wikipedia-preview/src/*.js
+cd wikipedia-preview
+git reset --hard
+npm i
+sed -i "s/inline-svg( '..\/images/url( wp-content\/plugins\/wikipediapreview-wordpress\/assets\/images/g" style/*.less
+sed -i "s/svg' )/svg )/g" style/*.less
+sed -i "s/url( '/url( /g" style/*.less
+sed -i "s/import '.*\/style\/.*'//g" src/**.js
+sed -i "s/import '.*\/style\/.*'//g" src/gallery/**.js
+# end of the replace method
 
-# cp node_modules/wikipedia-preview/dist/wikipedia-preview.production.js assets/js
+npm run build
+cd ..
+cp wikipedia-preview/dist/wikipedia-preview.production.js assets/js
