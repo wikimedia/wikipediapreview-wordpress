f1a3e34a220cb60611b79e0355017f91f31b0514
Huei Tan <huei90@gmail.com>
2021-05-05 15:32:28 +0200

    (wip) Convert all inline-svg( '../images/.svg' ) into url( assets/images/.svg )

--- scripts/postinstall.sh
+++ scripts/postinstall.sh
@@ -3,4 +3,11 @@
 mkdir -p assets/images
 
 cp wikipedia-preview/images/* assets/images
+
+# rewrite the replace method in a better format
+sed -i "s/inline-svg( '..\/images/url( wp-content\/plugins\/wikipediapreview-wordpress\/assets\/images/g" wikipedia-preview/style/*.less
+sed -i "s/svg' )/svg )/g" wikipedia-preview/style/*.less
+sed -i "s/url( '/url( /g" wikipedia-preview/style/*.less
+sed -i "s/import '..\/style\/.*.less'//g" wikipedia-preview/src/*.js
+
 # cp node_modules/wikipedia-preview/dist/wikipedia-preview.production.js assets/js
