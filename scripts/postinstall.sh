b5d1cd5dfbf2c6b611f98c10b17f3669fbeacd48
Huei Tan <huei90@gmail.com>
2021-05-06 13:18:18 +0200

    make postinstall.sh script readable

--- scripts/postinstall.sh
+++ scripts/postinstall.sh
@@ -1,28 +1,26 @@
 #!/bin/bash
 
-mkdir -p assets/images
-cp wikipedia-preview/images/* assets/images
-
-# rewrite the replace method in a better format
+# preparing compiled js and css file
 cd wikipedia-preview
 git reset --hard
 npm i
-sed -i "s/inline-svg( '..\/images/url( \/wp-content\/plugins\/wikipediapreview-wordpress\/assets\/images/g" style/*.less
-sed -i "s/svg' )/svg )/g" style/*.less
-sed -i "s/url( '/url( /g" style/*.less
-sed -i "s/import '.*\/style\/.*'//g" src/**.js
-sed -i "s/import '.*\/style\/.*'//g" src/gallery/**.js
-# end of the replace method
 
+# compiled js
+sed -i "/import '.*\/style\/.*'/d" src/*.js
+sed -i "/import '.*\/style\/.*'/d" src/**/*.js
 npm run build
 
+# compiled css
+sed -i "s/inline-svg( '..\/images\/\(.*\)' )/url( \/wp-content\/plugins\/wikipediapreview-wordpress\/assets\/images\/\1 )/g" style/*.less
+sed -i "s/url( '\(.*\)' )/url( \1 )/g" style/*.less
 echo '@import "gallery.less";@import "popup.less";@import "preview.less";' > style/index.less
 node node_modules/less/bin/lessc style/index.less dist/wikipedia-preview.production.css
 
 cd ..
 
-# moving js and css from wikipedia-preview into assets
+# moving assets from wikipedia-preview
+mkdir -p assets/{images,styles}
+cp wikipedia-preview/images/* assets/images
 cp wikipedia-preview/dist/wikipedia-preview.production.js assets/js
-mkdir -p assets/styles
 cp wikipedia-preview/dist/wikipedia-preview.production.css assets/styles
 
