fbe4cee9bf4e529ce27cd95ccc06c909a75adfbf
Huei Tan <huei90@gmail.com>
2021-05-11 11:41:43 +0200

    Update submodule and postinstall script

--- scripts/postinstall.sh
+++ scripts/postinstall.sh
@@ -1,27 +1,15 @@
 #!/bin/bash
 
-# preparing compiled js and css file
+# compiled wikipedia-preview
 git submodule update
 cd wikipedia-preview
 git reset --hard
 npm i
-
-# compiled js
-sed -i "/import '.*\/style\/.*'/d" src/*.js
-sed -i "/import '.*\/style\/.*'/d" src/**/*.js
-npm run build
-
-# compiled css
-sed -i "s|inline-svg( '../images/\(.*\)' )|url( /wp-content/plugins/wikipediapreview-wordpress/assets/images/\1 )|g" style/*.less
-sed -i "s|url( '\(.*\)' )|url( \1 )|g" style/*.less
-echo '@import "gallery.less";@import "popup.less";@import "preview.less";' > style/index.less
-node node_modules/less/bin/lessc style/index.less dist/wikipedia-preview.production.css
-
+EXTERNAL_IMAGE_PATH='/wp-content/plugins/wikipediapreview-wordpress/assets/images' npm run build
 cd ..
 
 # moving assets from wikipedia-preview
-mkdir -p assets/{images,styles}
+mkdir -p assets/images
 cp wikipedia-preview/images/* assets/images
 cp wikipedia-preview/dist/wikipedia-preview.production.js assets/js
-cp wikipedia-preview/dist/wikipedia-preview.production.css assets/styles
 
