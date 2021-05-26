b3e7841df33bc646070ae8012962da3d0b1d96a1
Huei Tan <huei90@gmail.com>
2021-05-26 16:49:11 +0200

    Add php linter and script

--- wikipediapreview.php
+++ wikipediapreview.php
@@ -11,8 +11,9 @@
  * License: MIT
  * License URI: https://github.com/wikimedia/wikipedia-preview/blob/main/LICENSE
  */
-function wikipediapreview_enqueue_scripts() {
-    $assets_dir = plugin_dir_url( __FILE__ ) . 'assets/';
+function wikipediapreview_enqueue_scripts()
+{
+    $assets_dir = plugin_dir_url(__FILE__) . 'assets/';
 
     wp_enqueue_script(
         'wikipedia-preview', $assets_dir . 'js/wikipedia-preview.production.js', [], false, true
@@ -25,14 +26,16 @@ function wikipediapreview_enqueue_scripts() {
 
 // Record the option of detect links feature enabled in this version,
 // detect links feature may be disabled by default in the next version
-function wikipediapreview_detect_true() {
-    add_option( 'wikipediapreview_options_detect_links', true );
+function wikipediapreview_detect_true()
+{
+    add_option('wikipediapreview_options_detect_links', true);
 }
 
-function wikipediapreview_detect_deletion() {
-    delete_option( 'wikipediapreview_options_detect_links' );
+function wikipediapreview_detect_deletion()
+{
+    delete_option('wikipediapreview_options_detect_links');
 }
 
-register_activation_hook( __FILE__, 'wikipediapreview_detect_true' );
-register_deactivation_hook( __FILE__, 'wikipediapreview_detect_deletion' );
-add_action( 'wp_enqueue_scripts', 'wikipediapreview_enqueue_scripts' );
+register_activation_hook(__FILE__, 'wikipediapreview_detect_true');
+register_deactivation_hook(__FILE__, 'wikipediapreview_detect_deletion');
+add_action('wp_enqueue_scripts', 'wikipediapreview_enqueue_scripts');
