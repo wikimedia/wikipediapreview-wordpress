63b21b3aa9c7587879e2d39a587138fc9c3bb1b6
Stephane Bisson <sbisson@wikimedia.org>
2021-08-19 11:57:37 -0400

    Make sure to create build and libs dirs

--- scripts/publish.sh
+++ scripts/publish.sh
@@ -13,7 +13,9 @@ npm install
 npm run build
 cp ./wikipediapreview.php $1/trunk/
 cp ./readme.txt $1/trunk/
+mkdir $1/trunk/build
 cp ./build/{index.js,init.js,style-index.css} $1/trunk/build
+mkdir $1/trunk/libs
 cp ./libs/{wikipedia-preview-link.css,wikipedia-preview.production.js} $1/trunk/libs
 
 cd $1
