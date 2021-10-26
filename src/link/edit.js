41ff8d4af377de7ea1fbb3f3d4977db339efad15
Huei Tan <huei90@gmail.com>
2021-10-26 18:25:20 +0200

    Update svg viewBox

--- src/link/edit.js
+++ src/link/edit.js
@@ -17,9 +17,7 @@ const formatTitle = __( 'Wikipedia Preview', 'wikipedia-preview' );
 const generateWikipediaLogo = ( color = 'black' ) => {
 	return (
 		<svg
-			width="20"
-			height="20"
-			viewBox="0 0 20 20"
+			viewBox="-2 -2 24 24"
 			fill="none"
 			xmlns="http://www.w3.org/2000/svg"
 		>
