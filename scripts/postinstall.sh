28c4fd4fa02b241e45a3346b7320d5afec580e30
medied <medina.eag@gmail.com>
2023-02-15 10:33:04 -0400

    Add wordpress class selector

--- scripts/postinstall.sh
+++ scripts/postinstall.sh
@@ -3,4 +3,5 @@
 mkdir libs
 cp node_modules/wikipedia-preview/dist/wikipedia-preview.production.js libs/
 cp node_modules/wikipedia-preview/dist/wikipedia-preview-link.css libs/
+echo ".wp-block {}" >> libs/wikipedia-preview-link.css # Needed to allow .wmf-wp-* classes to be available inside Wordpress editor
 npm run build
