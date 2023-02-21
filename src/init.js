ab268c711def7a67ad1a8881481bf8baa2071e09
Huei Tan <hueitan@users.noreply.github.com>
2023-02-21 16:58:44 +0100

    T329753 update dependencies (#75)
    * update dependencies
    
    * Address lint errors
    
    * Address lint errors
    
    * Style lint only for css files
    
    * Add stylelintignore
    
    * New line missing, next-line attempt
    
    ---------
    
    Co-authored-by: medied <medina.eag@gmail.com>

--- src/init.js
+++ src/init.js
@@ -1,5 +1,6 @@
 /* global wikipediapreview_init_options */
 wikipediaPreview.init( {
 	root: document,
+	/* eslint-disable-next-line camelcase */
 	detectLinks: !! wikipediapreview_init_options.detectLinks,
 } );
