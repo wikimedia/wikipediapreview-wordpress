877ae07726819cb94a282a80d9c9b7e7a89eb475
Stephane Bisson <stephane.c.bisson@gmail.com>
2023-06-28 13:43:53 -0400

    Fix WP style filename in postinstall script (#86)
    Co-authored-by: Stephane Bisson <sbisson@wikimedia.org>

--- scripts/postinstall.sh
+++ scripts/postinstall.sh
@@ -2,6 +2,6 @@
 
 mkdir libs
 cp node_modules/wikipedia-preview/dist/wikipedia-preview.production.js libs/
-cp node_modules/wikipedia-preview/dist/wikipedia-preview-link.css libs/
+cp node_modules/wikipedia-preview/dist/wikipedia-preview.css libs/wikipedia-preview-link.css
 echo ".wp-block {}" >> libs/wikipedia-preview-link.css # Needed to allow .wmf-wp-* classes to be available inside Wordpress editor
 npm run build
