fe2a4947d94abfe12892cc4dd7105eaf9989c6f0
Stephane Bisson <stephane.c.bisson@gmail.com>
2023-11-07 09:46:34 -0500

    Enable Wikipedia Preview on all public post types (#98)
    * Enable Wikipedia Preview on all public post types
    
    * Cleanup style
    
    ---------
    
    Co-authored-by: Sbisson <sbisson@wikimedia.org>

--- wikipediapreview.php
+++ wikipediapreview.php
@@ -15,8 +15,19 @@
 
 DEFINE( 'WIKIPEDIA_PREVIEW_PLUGIN_VERSION', '1.11.0' );
 
+function get_public_post_types() {
+	$post_types        = get_post_types( array(), 'objects' );
+	$public_post_types = array();
+	foreach ( $post_types as $post_type ) {
+		if ( $post_type->public ) {
+			$public_post_types[] = $post_type->name;
+		}
+	}
+	return $public_post_types;
+}
+
 function wikipediapreview_enqueue_scripts() {
-	if ( ! in_array( get_post_type(), array( 'post', 'page' ), true ) ) {
+	if ( ! in_array( get_post_type(), get_public_post_types(), true ) ) {
 		return;
 	}
 	$build_dir       = plugin_dir_url( __FILE__ ) . 'build/';
@@ -63,7 +74,7 @@ function wikipediapreview_detect_deletion() {
 }
 
 function wikipediapreview_guten_enqueue() {
-	if ( ! in_array( get_post_type(), array( 'post', 'page' ), true ) ) {
+	if ( ! in_array( get_post_type(), get_public_post_types(), true ) ) {
 		return;
 	}
 	$build_dir       = plugin_dir_url( __FILE__ ) . 'build/';
