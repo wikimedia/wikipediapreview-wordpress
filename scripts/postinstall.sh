cea0e51feeb2be7b944d3650861a0cea36a49a36
Huei Tan <huei90@gmail.com>
2023-11-30 10:34:05 +0100

    update post install script;

--- scripts/postinstall.sh
+++ scripts/postinstall.sh
@@ -1,7 +1,12 @@
 #!/bin/bash
 
 mkdir libs
+
+# wikipedia preview core script
 cp node_modules/wikipedia-preview/dist/wikipedia-preview.umd.cjs libs/wikipedia-preview.js
-cp node_modules/wikipedia-preview/dist/wikipedia-preview.css libs/wikipedia-preview-link.css
+
+# default preview link style
+cp node_modules/wikipedia-preview/demo/css/link.css libs/wikipedia-preview-link.css
+
 echo ".wp-block {}" >> libs/wikipedia-preview-link.css # Needed to allow .wmf-wp-* classes to be available inside Wordpress editor
 npm run build
