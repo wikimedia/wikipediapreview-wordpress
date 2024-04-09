cf59256024d5d6149eac265fcea42e4c7111d03a
eamedina <eamedina@wikimedia.org>
2024-04-09 12:42:59 -0400

    Address php linter

--- tooltip.php
+++ tooltip.php
@@ -3,23 +3,29 @@
 DEFINE( 'WIKIPEDIA_PREVIEW_TOOLTIP_DISPLAYED_COUNT', 'wikipediapreview_tooltip_count' );
 
 function wikipediapreview_get_tooltip_count() {
-    return get_option( WIKIPEDIA_PREVIEW_TOOLTIP_DISPLAYED_COUNT, 0 );
+	return get_option(
+		WIKIPEDIA_PREVIEW_TOOLTIP_DISPLAYED_COUNT,
+		0
+	);
 }
 
 function wikipediapreview_increment_tooltip_count() {
-    $count = wikipediapreview_get_tooltip_count();
-    update_option( WIKIPEDIA_PREVIEW_TOOLTIP_DISPLAYED_COUNT, $count + 1 );
+	$count = wikipediapreview_get_tooltip_count();
+	update_option(
+		WIKIPEDIA_PREVIEW_TOOLTIP_DISPLAYED_COUNT,
+		$count + 1
+	);
 }
 
 add_action('rest_api_init', function () {
-    $route_namespace = 'wikipediapreview/v1';
+	$route_namespace = 'wikipediapreview/v1';
 
-    register_rest_route($route_namespace, '/option/', array(
-        'methods' => 'GET',
-        'callback' => 'wikipediapreview_get_tooltip_count',
-    ));
-    register_rest_route($route_namespace, '/option/', array(
-        'methods' => 'POST',
-        'callback' => 'wikipediapreview_increment_tooltip_count',
-    ));
+	register_rest_route( $route_namespace, '/option/', array(
+		'methods'  => 'GET',
+		'callback' => 'wikipediapreview_get_tooltip_count',
+	) );
+	register_rest_route( $route_namespace, '/option/', array(
+		'methods'  => 'POST',
+		'callback' => 'wikipediapreview_increment_tooltip_count',
+	) );
 });
