f0e8597fe6472920b07d15685953c1422e62247c
Huei Tan <huei90@gmail.com>
2024-06-26 14:22:28 +0200

    add z-index to fullscreen gallery

--- scripts/postinstall.sh
+++ scripts/postinstall.sh
@@ -12,4 +12,5 @@ cp node_modules/wikipedia-preview/dist/wikipedia-preview.css libs/wikipedia-prev
 cp node_modules/wikipedia-preview/dist/wikipedia-preview-link.css libs/wikipedia-preview-link.css
 
 echo ".wp-block {}" >> libs/wikipedia-preview-link.css # Needed to allow .wmf-wp-* classes to be available inside Wordpress editor
+echo ".wp-gallery-fullscreen { z-index: 100000 !important; }" >> libs/wikipedia-preview-link.css # the value must greater than wordpress header to show fullscreen gallery
 npm run build
