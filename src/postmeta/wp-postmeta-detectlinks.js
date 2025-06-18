67ee70cfa8c8ad40e9c8b1a2c1793c63dd132c19
Huei Tan <hueitan@users.noreply.github.com>
2025-06-19 03:08:19 +0800

    Follow up from Adjust PluginDocumentSettingsPanel usage for WP 6.6+ compatibility #141 (#142)
    * Adjust document setting panel slot import for WP 6.6+ compatibility
    
    This module has been moved to the @wordpress/editor package:
    https://developer.wordpress.org/block-editor/reference-guides/slotfills/plugin-document-setting-panel/
    
    * Add required "name" property to PluginDocumentSettingsPanel instance
    
    * Tested up to: 6.7.2
    
    * 1.15.0
    
    * Revert "1.15.0"
    
    This reverts commit 768ea90a999af0bcc1a495fcabce82466eb8df9d.
    
    * Revert "Tested up to: 6.7.2"
    
    This reverts commit 95f7b637ad325ce236d0a9bab957f43ecf6514d4.
    
    * add @wordpress/editor
    
    * backward compatibility for wordpress <6.6
    
    https://make.wordpress.org/core/2024/06/18/editor-unified-extensibility-apis-in-6-6/
    
    ---------
    
    Co-authored-by: K. Adam White <kadamwhite@users.noreply.github.com>

--- src/postmeta/wp-postmeta-detectlinks.js
+++ src/postmeta/wp-postmeta-detectlinks.js
@@ -1,7 +1,6 @@
 import { __ } from '@wordpress/i18n';
 import { compose } from '@wordpress/compose';
 import { withSelect, withDispatch } from '@wordpress/data';
-import { PluginDocumentSettingPanel } from '@wordpress/edit-post';
 import { ToggleControl, PanelRow } from '@wordpress/components';
 
 const WikipediaPreviewPostMetaDetectLinks = ( { postMeta, setPostMeta } ) => {
@@ -10,8 +9,18 @@ const WikipediaPreviewPostMetaDetectLinks = ( { postMeta, setPostMeta } ) => {
 		// without 'custom-fields' support
 		return;
 	}
+
+	// https://make.wordpress.org/core/2024/06/18/editor-unified-extensibility-apis-in-6-6/
+	const PluginDocumentSettingPanel = wp.editor?.PluginDocumentSettingPanel ??
+		( wp.editPost?.PluginDocumentSettingPanel ?? wp.editSite?.PluginDocumentSettingPanel );
+
+	if ( ! PluginDocumentSettingPanel ) {
+		return null;
+	}
+
 	return (
 		<PluginDocumentSettingPanel
+			name="wikipedia-preview"
 			title={ __( 'Wikipedia Preview', 'wikipedia-preview' ) }
 			initialOpen="false"
 		>
